01fdde439af7d88d40b0abf89c047cf811fcbeea
benaor <benjamingirard25@gmail.com>
2020-08-20 14:32:13 +0200

    read and close the txt file

--- index.php
+++ index.php
@@ -3,9 +3,13 @@
     // 'a+' for create, read and edit. 
     // else i can use r+ if the file already exist, for read et edit.
     // 'r' and 'a' is possible for just read the file (and create in the case of 'a' )
-    $fichier = fopen('visitor_counter.txt', 'r+');
+    $file = fopen('visitor_counter.txt', 'r+');
 
-    fclose($fichier);
+    //Read the file
+    $visitor = fgets($file);
+
+    //close the file
+    fclose($file);
 ?>
 
 <!DOCTYPE html>
