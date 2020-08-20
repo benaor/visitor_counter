a6bdd009d5f7b76936abdb2dcb1f781e9b619c19
benaor <benjamingirard25@gmail.com>
2020-08-20 14:22:40 +0200

    create file counter_visitor.txt with command 'a+'

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php 
 
+    // 'a+' for create, read and edit. 
+    // else i can use r+ if the file already exist, for read et edit.
+    // 'r' and 'a' is possible for just read the file (and create in the case of 'a' )
+    $fichier = fopen('visitor_counter.txt', 'r+');
+
+    fclose($fichier);
 ?>
 
 <!DOCTYPE html>
